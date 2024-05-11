d6543ef995708f19eb030dfdb84f7cfadd1edd7a
burn.s2813 <burn.s2813@gmail.com>
2024-05-11 11:51:24 +0000

    Add Navbar and install svelete carousel

---
+++ src/routes/(app)/home/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="js">
+// @ts-nocheck
+
+    import Navbar from '$lib/Navbar/Navbar.svelte'
+    import Carousel from 'svelte-carousel';
+    import { browser } from '$app/environment';
+  
+    let carousel ; // for calling methods of the carousel instance
+    const handleNextClick = () => {
+      carousel.goToNext()
+    }
+  </script>
+  
+  {#if browser}
+    <Navbar />
+
+    <Carousel
+      bind:this={carousel}
+    >
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+    </Carousel>
+  {/if}
+  
+  <button on:click={handleNextClick}>Next</button>
